e6bb359e3e051d55cab2931dbd5bef858aa0b612
skmbw <tongku2008@126.com>
2014-10-17 17:46:06 +0800

    压缩工具

---
+++ vteba-utils/src/main/java/com/vteba/utils/common/ZipUtils.java
@@ -0,0 +1,383 @@
+package com.vteba.utils.common;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vteba.utils.charstr.Char;
+
+/**
+ * 常用的压缩工具。包括GZIP ZIP ZLIB。默认编码是UTF-8
+ * @author YINLEI 
+ * @since 2014-1-1
+ */
+public class ZipUtils {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ZipUtils.class);
+
+	/**
+	 * 使用gzip进行压缩
+	 * 
+	 * @param data
+	 *            待压缩的字符串
+	 * @return 压缩后的字符串
+	 */
+	public static String gzips(String data) {
+		byte[] result = gzip(data);
+		return new String(result, Char.UTF8);
+	}
+
+	/**
+	 * 使用gzip进行压缩
+	 * 
+	 * @param data
+	 *            待压缩的字符串
+	 * @return 压缩后的字节数组
+	 */
+	public static byte[] gzip(String data) {
+		byte[] dataBytes = data.getBytes(Char.UTF8);
+		return gzip(dataBytes);
+	}
+
+	/**
+	 * 使用gzip进行压缩
+	 * 
+	 * @param data
+	 *            待压缩的数据
+	 * @return 压缩后的字节数组
+	 */
+	public static byte[] gzip(byte[] data) {
+		if (data == null || data.length == 0) {
+			return null;
+		}
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		GZIPOutputStream gzip = null;
+		try {
+			gzip = new GZIPOutputStream(out);
+			gzip.write(data);
+		} catch (IOException e) {
+			LOGGER.error("gzip压缩字符IO错误。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(gzip);
+			IOUtils.closeQuietly(out);
+		}
+		return out.toByteArray();
+	}
+	
+	/**
+	 * 使用gzip进行解压缩
+	 * 
+	 * @param data
+	 *            待解压的字符
+	 * @return 解压后的字符
+	 */
+	public static String ungzip(String data) {
+		if (data == null) {
+			return null;
+		}
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ByteArrayInputStream in = null;
+		GZIPInputStream ginzip = null;
+		byte[] compBytes = null;
+		String result = null;
+		try {
+			compBytes = data.getBytes(Char.UTF8);
+			in = new ByteArrayInputStream(compBytes);
+			ginzip = new GZIPInputStream(in);
+
+			byte[] buffer = new byte[1024];
+			int offset = -1;
+			while ((offset = ginzip.read(buffer)) != -1) {
+				out.write(buffer, 0, offset);
+			}
+			result = out.toString("UTF-8");
+		} catch (IOException e) {
+			LOGGER.error("gzip解压字符IO错误。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(ginzip);
+			IOUtils.closeQuietly(in);
+			IOUtils.closeQuietly(out);
+		}
+		return result;
+	}
+
+	/**
+	 * 使用zip进行压缩
+	 * 
+	 * @param str
+	 *            压缩前的文本
+	 * @return 返回压缩后的文本
+	 */
+	public static final String zip(String str) {
+		if (str == null) {
+			return null;
+		}
+		byte[] compressed;
+		ByteArrayOutputStream out = null;
+		ZipOutputStream zout = null;
+		String compressedStr = null;
+		try {
+			out = new ByteArrayOutputStream();
+			zout = new ZipOutputStream(out);
+			zout.putNextEntry(new ZipEntry("0"));
+			zout.write(str.getBytes());
+			zout.closeEntry();
+			compressed = out.toByteArray();
+			compressedStr = new String(compressed, Char.UTF8);
+		} catch (IOException e) {
+			compressed = null;
+			LOGGER.error("zip压缩字符IO错误。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(zout);
+			IOUtils.closeQuietly(out);
+		}
+		return compressedStr;
+	}
+
+	/**
+	 * 使用zip进行解压
+	 * 
+	 * @param data
+	 *            待解压的文本
+	 * @return 解压后的字符串
+	 */
+	public static final String unzip(String data) {
+		if (data == null) {
+			return null;
+		}
+
+		ByteArrayOutputStream out = null;
+		ByteArrayInputStream in = null;
+		ZipInputStream zin = null;
+		String decompressed = null;
+		try {
+			byte[] compressed = data.getBytes(Char.UTF8);
+			out = new ByteArrayOutputStream();
+			in = new ByteArrayInputStream(compressed);
+			zin = new ZipInputStream(in);
+			zin.getNextEntry();
+			byte[] buffer = new byte[1024];
+			int offset = -1;
+			while ((offset = zin.read(buffer)) != -1) {
+				out.write(buffer, 0, offset);
+			}
+			decompressed = out.toString();
+		} catch (IOException e) {
+			decompressed = null;
+			LOGGER.error("zip解压字符IO错误。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(zin);
+			IOUtils.closeQuietly(in);
+			IOUtils.closeQuietly(out);
+		}
+		return decompressed;
+	}
+
+	/**
+	 * zlib压缩数据
+	 * 
+	 * @param data
+	 *            待压缩数据
+	 * @return byte[] 压缩后的数据
+	 */
+	public static byte[] zlib(byte[] data) {
+		byte[] output = new byte[0];
+
+		Deflater compresser = new Deflater();
+
+		compresser.reset();
+		compresser.setInput(data);
+		compresser.finish();
+		ByteArrayOutputStream bos = new ByteArrayOutputStream(data.length);
+		try {
+			byte[] buf = new byte[1024];
+			while (!compresser.finished()) {
+				int i = compresser.deflate(buf);
+				bos.write(buf, 0, i);
+			}
+			output = bos.toByteArray();
+		} catch (Exception e) {
+			LOGGER.error("zlib压缩数据异常。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(bos);
+		}
+		compresser.end();
+		return output;
+	}
+
+	/**
+	 * zlib压缩数据
+	 * 
+	 * @param data
+	 *            待压缩数据
+	 * @return byte[] 压缩后的数据
+	 */
+	public static String zlibString(byte[] data) {
+		String output = null;
+
+		Deflater compresser = new Deflater();
+
+		compresser.reset();
+		compresser.setInput(data);
+		compresser.finish();
+		ByteArrayOutputStream bos = new ByteArrayOutputStream(data.length);
+		try {
+			byte[] buf = new byte[1024];
+			while (!compresser.finished()) {
+				int i = compresser.deflate(buf);
+				bos.write(buf, 0, i);
+			}
+			output = bos.toString("UTF-8");
+		} catch (Exception e) {
+			LOGGER.error("zlib压缩数据异常。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(bos);
+		}
+		compresser.end();
+		return output;
+	}
+	
+	/**
+	 * zlib压缩数据
+	 * 
+	 * @param data
+	 *            待压缩数据
+	 * @return byte[] 压缩后的数据
+	 */
+	public static byte[] zlibByte(String data) {
+		byte[] dataByte = data.getBytes(Char.UTF8);
+		return zlib(dataByte);
+	}
+	
+	/**
+	 * zlib压缩数据
+	 * 
+	 * @param data
+	 *            待压缩数据
+	 * 
+	 * @param os
+	 *            输出流
+	 */
+	public static OutputStream zlibs(byte[] data) {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		DeflaterOutputStream dos = new DeflaterOutputStream(os);
+
+		try {
+			dos.write(data, 0, data.length);
+			dos.finish();
+			dos.flush();
+		} catch (IOException e) {
+			LOGGER.error("zlib压缩数据异常。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(dos);
+		}
+		return os;
+	}
+
+	/**
+	 * zlib压缩数据
+	 * 
+	 * @param data
+	 *            待压缩数据
+	 * @return String 压缩后的数据
+	 */
+	public static String zlibs(String data) {
+		String output = null;
+		Deflater compresser = new Deflater();
+		byte[] dataByte = data.getBytes(Char.UTF8);
+		compresser.reset();
+		compresser.setInput(dataByte);
+		compresser.finish();
+		ByteArrayOutputStream bos = new ByteArrayOutputStream(dataByte.length);
+		try {
+			byte[] buf = new byte[1024];
+			while (!compresser.finished()) {
+				int i = compresser.deflate(buf);
+				bos.write(buf, 0, i);
+			}
+			output = bos.toString("UTF-8");
+		} catch (Exception e) {
+			LOGGER.error("zlib压缩数据异常。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(bos);
+		}
+		compresser.end();
+		return output;
+	}
+	
+	/**
+	 * zlib解压缩字节数组
+	 * 
+	 * @param data
+	 *            待解压缩的数据
+	 * @return byte[] 解压缩后的数据
+	 */
+	public static byte[] unzlib(byte[] data) {
+		byte[] output = new byte[0];
+
+		Inflater decompresser = new Inflater();
+		decompresser.reset();
+		decompresser.setInput(data);
+
+		ByteArrayOutputStream o = new ByteArrayOutputStream(data.length);
+		try {
+			byte[] buf = new byte[1024];
+			while (!decompresser.finished()) {
+				int i = decompresser.inflate(buf);
+				o.write(buf, 0, i);
+			}
+			output = o.toByteArray();
+		} catch (Exception e) {
+			output = data;
+			LOGGER.error("zlib解压数据异常。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(o);
+		}
+		decompresser.end();
+		return output;
+	}
+
+	/**
+	 * zlib解压数据流
+	 * 
+	 * @param is
+	 *            输入流
+	 * @return byte[] 解压缩后的数据
+	 */
+	public static byte[] unzlib(InputStream is) {
+		byte[] output = new byte[0];
+		InflaterInputStream iis = new InflaterInputStream(is);
+		ByteArrayOutputStream o = new ByteArrayOutputStream(1024);
+		try {
+			int i = 0;
+			byte[] buf = new byte[1024];
+
+			while ((i = iis.read(buf, 0, i)) > 0) {
+				o.write(buf, 0, i);
+			}
+			output = o.toByteArray();
+			o.flush();
+		} catch (IOException e) {
+			LOGGER.error("zlib解压数据异常。", e.getMessage());
+		} finally {
+			IOUtils.closeQuietly(o);
+			IOUtils.closeQuietly(iis);
+		}
+		return output;
+	}
+}
