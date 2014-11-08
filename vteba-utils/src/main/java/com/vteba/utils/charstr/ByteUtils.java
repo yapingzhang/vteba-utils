0a302a7108d374f1dbfb57d66bdcaa3a2ef35890
skmbw <tongku2008@126.com>
2014-11-08 11:53:15 +0800

    16进制字节数组和int转换

--- vteba-utils/src/main/java/com/vteba/utils/charstr/ByteUtils.java
+++ vteba-utils/src/main/java/com/vteba/utils/charstr/ByteUtils.java
@@ -90,6 +90,79 @@ public class ByteUtils {
 		return targets;
 	}
 
+	/** 
+     * 将指定byte数组以16进制的形式打印到控制台 
+     * @param hint String 
+     * @param b byte[] 
+     * @return void 
+     */ 
+    public static String printHexString( byte[] b) {
+        StringBuffer returnValue = new StringBuffer();
+        for (int i = 0; i < b.length; i++) {
+            String hex = Integer.toHexString(b[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            System.out.print(hex.toUpperCase() + " ");
+            returnValue.append(hex.toUpperCase() + " ");
+        }
+
+        return "[" + returnValue.toString() + "]";
+    }
+
+    /** 
+     *
+     * @param b byte[]
+     * @return String
+     */
+    public static String byte2Hex(byte[] b) {
+        String ret = "";
+        for (int i = 0; i < b.length; i++) {
+            String hex = Integer.toHexString(b[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            ret += hex.toUpperCase();
+        }
+        return ret;
+    }
+
+    /** 
+     * 将两个ASCII字符合成一个字节； 
+     * 如："EF"--> 0xEF 
+     * @param src0 byte 
+     * @param src1 byte 
+     * @return byte 
+     */ 
+    public static byte uniteBytes(byte src0, byte src1) {
+        byte _b0 = Byte.decode("0x" + new String(new byte[]{src0})).byteValue();
+        _b0 = (byte) (_b0 << 4);
+        byte _b1 = Byte.decode("0x" + new String(new byte[]{src1})).byteValue();
+        byte ret = (byte) (_b0 ^ _b1);
+        return ret;
+    }
+
+    /** 
+     * 将指定字符串src，以每两个字符分割转换为16进制形式 
+     * 如："2B44EFD9" --> byte[]{0x2B, 0x44, 0xEF, 0xD9} 
+     * @param src String 
+     * @return byte[] 
+     */ 
+    public static byte[] hex2Byte(String src) {
+        byte[] ret = new byte[8];
+        byte[] tmp = src.getBytes();
+        for (int i = 0; i < 8; i++) {
+            ret[i] = uniteBytes(tmp[i * 2], tmp[i * 2 + 1]);
+        }
+        return ret;
+    }
+    
+    public static int hexByte2Int(byte[] bytes) {
+    	String aaa = byte2Hex(bytes);
+    	int aa = Integer.parseInt(aaa, 16);
+    	return aa;
+    }
+	
 	public static void main(String[] args) {
 		toBytes("a尹雷", "b");
 		
